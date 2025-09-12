8df6eac19ac5568bb94002b2638f82edb26e8e14
Goofy <gfxayrton@hotmail.com>
2025-09-12 02:24:29 -0300

    trying out audio

--- Proyecto Grupo 3/Assets/Scripts/Character Selection/CharacterCreator.cs
+++ Proyecto Grupo 3/Assets/Scripts/Character Selection/CharacterCreator.cs
@@ -4,23 +4,22 @@ using System.Collections.Generic;
 // genera los personajes en la escena
 public class CharacterCreator : MonoBehaviour
 {
+    [SerializeField] public List<GameObject> characters;
 
-  [SerializeField] public List<GameObject> characters;
-
-  public void 
-        CreateCharacter(Block clicked, int characterID, int player)
-  {
-    if (characterID != -1)
+    public void
+          CreateCharacter(Block clicked, int characterID, int player)
     {
-      GameObject characterPrefab = characters[characterID];
-      GameObject localChara = Instantiate(characterPrefab, new Vector3(clicked.transform.position.x, clicked.transform.position.y + 1.5f + clicked.height, clicked.transform.position.z), Quaternion.identity);
-      localChara.GetComponent<CharacterController>().belongsToPlayer = player;
-      Transform lookingAt = localChara.transform.Find("LookingAt").transform;
-      if (player == 1)
-        lookingAt.rotation = Quaternion.LookRotation(new Vector3(1, 0, 0), lookingAt.up);
-      else
-        lookingAt.rotation = Quaternion.LookRotation(new Vector3(-1, 0, 0), lookingAt.up);
+        if (characterID != -1)
+        {
+            GameObject characterPrefab = characters[characterID];
+            GameObject localChara = Instantiate(characterPrefab, new Vector3(clicked.transform.position.x, clicked.transform.position.y + 1.5f + clicked.height, clicked.transform.position.z), Quaternion.identity);
+            localChara.GetComponent<CharacterController>().belongsToPlayer = player;
+            Transform lookingAt = localChara.transform.Find("LookingAt").transform;
+            if (player == 1)
+                lookingAt.rotation = Quaternion.LookRotation(new Vector3(1, 0, 0), lookingAt.up);
+            else
+                lookingAt.rotation = Quaternion.LookRotation(new Vector3(-1, 0, 0), lookingAt.up);
+        }
     }
-  }
 
 }
--- Proyecto Grupo 3/Assets/Scripts/Character Selection/CharacterSelector.cs
+++ Proyecto Grupo 3/Assets/Scripts/Character Selection/CharacterSelector.cs
@@ -1,51 +1,53 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class CharacterSelector : MonoBehaviour
 {
-  private int selectedCharacterID = -1;
-  private CharacterCreator charCreator;
+    private int selectedCharacterID = -1;
+    private CharacterCreator charCreator;
 
-  [SerializeField] private CinemachineCamera vcam;
-  [SerializeField] private GameObject midMapTarget;
+    [SerializeField] private CinemachineCamera vcam;
+    [SerializeField] private GameObject midMapTarget;
+    [SerializeField] private AudioSource m_CharacterSelectedSFX;
+    //player elige sus unidades, se agregan a una lista de CharacterController(s) correspondientes a ese jugador
+    private int charactersLeft = 4;
+    private int currentPlayer = 1;
+    private int maxPlayers = 2;
 
-  //player elige sus unidades, se agregan a una lista de CharacterController(s) correspondientes a ese jugador
-  private int charactersLeft = 4;
-  private int currentPlayer = 1;
-  private int maxPlayers = 2;
-
-  public void Start()
-  {
-    charCreator = FindFirstObjectByType<CharacterCreator>();
-  }
-  
-  public void OnCharacterSelected(int characterID)
-  {
-    if (selectedCharacterID == -1)
+    public void Start()
     {
-      selectedCharacterID = characterID;
-      Block.onBlockClicked += OnCharacterPlaced;
+        charCreator = FindFirstObjectByType<CharacterCreator>();
     }
-    else selectedCharacterID = characterID;
-    //Debug.Log("seleccionaste " + characterID);
-  }
 
-  public void OnCharacterPlaced(Block clicked)
-  {
-      Block.onBlockClicked -= OnCharacterPlaced;
-      charCreator.CreateCharacter(clicked,selectedCharacterID,currentPlayer);
-      selectedCharacterID = -1;
-      charactersLeft--;
-      if(charactersLeft == 0)
-          {
-              currentPlayer++;
-              vcam.transform.position = new Vector3(26, 6, 13);
-              vcam.transform.LookAt(midMapTarget.transform);
-              if (currentPlayer > maxPlayers)
-              {
-                  UIManager.instance.DeactivateSelectorPanel();
-              }
-              else { UIManager.instance.ChangeSelectingPlayer(); charactersLeft = 4; }
-          }
-  }
+    public void OnCharacterSelected(int characterID)
+    {
+        if (selectedCharacterID == -1)
+        {
+            selectedCharacterID = characterID;
+            Block.onBlockClicked += OnCharacterPlaced;
+        }
+        else selectedCharacterID = characterID;
+        //Debug.Log("seleccionaste " + characterID);
+    }
+
+    public void OnCharacterPlaced(Block clicked)
+    {
+        Block.onBlockClicked -= OnCharacterPlaced;
+        charCreator.CreateCharacter(clicked, selectedCharacterID, currentPlayer);
+        selectedCharacterID = -1;
+        charactersLeft--;
+        m_CharacterSelectedSFX.Play();
+        if (charactersLeft == 0)
+        {
+            currentPlayer++;
+            vcam.transform.position = new Vector3(26, 6, 13);
+            vcam.transform.LookAt(midMapTarget.transform);
+            if (currentPlayer > maxPlayers)
+            {
+                UIManager.instance.DeactivateSelectorPanel();
+            }
+            else { UIManager.instance.ChangeSelectingPlayer(); charactersLeft = 4; }
+        }
+    }
 }
